2f01a06b55cedd5af04348c5a751d44f628a9564
Alisa-Shevchenko <alisa.shevchenko747@gmail.com>
2023-11-20 11:42:22 +0200

    Change selectors

--- cypress/e2e/app_tests/Add_a_new_row_to_the_table.cy.js
+++ cypress/e2e/app_tests/Add_a_new_row_to_the_table.cy.js
@@ -6,13 +6,13 @@
 describe('Table validation', () => {
     it('add a new row to the table', () => {
         cy.visit('/pages/tables/smart-table');
-        cy.get('.ng2-smart-actions-title > .ng2-smart-action').click();
-        cy.get(':nth-child(2) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("70");
-        cy.get(':nth-child(3) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("Alisa");
-        cy.get(':nth-child(4) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("Shevchenko");
-        cy.get(':nth-child(5) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("@alisa");
-        cy.get(':nth-child(6) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("alisa@mail.com");
-        cy.get(':nth-child(7) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').type("28");
+        cy.get('.nb-plus').click();
+        cy.get('input-editor [placeholder="ID"]').type("70");
+        cy.get('input-editor [placeholder="First Name"]').type("Alisa");
+        cy.get('input-editor [placeholder="Last Name"]').type("Shevchenko");
+        cy.get('input-editor [placeholder="Username"]').type("@alisa");
+        cy.get('input-editor [placeholder="E-mail"]').type("alisa@mail.com");
+        cy.get('input-editor [placeholder="Age"]').type("28");
         cy.get('.nb-checkmark').click()
         
         const elems = []
--- cypress/e2e/app_tests/Edit_a_row_from_a_table.cy.js
+++ cypress/e2e/app_tests/Edit_a_row_from_a_table.cy.js
@@ -5,9 +5,9 @@
 describe('Table validation', () => {
     it('edit a row from a table', () => {
         cy.visit('/pages/tables/smart-table');
-        cy.get(':nth-child(1) > .ng2-smart-actions > ng2-st-tbody-edit-delete > .ng2-smart-action-edit-edit > .nb-edit').click();
-        cy.get(':nth-child(2) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').clear().type("101");
-        cy.get(':nth-child(3) > ng2-smart-table-cell > table-cell-edit-mode.ng-star-inserted > :nth-child(1) > table-cell-default-editor.ng-star-inserted > div > .ng-star-inserted > .form-control').clear().type("Test");
+        cy.get('.nb-edit').first().click();
+        cy.get('input-editor [placeholder="ID"]').clear().type("101");
+        cy.get('input-editor [placeholder="First Name"]').clear().type("Test");
         cy.get('.nb-checkmark').click();
 
         const elems = []
